0b82462593b53cdff0eb7cea79bb3f8115114072
Anti1i <152875540+Anti1i@users.noreply.github.com>
2025-12-20 20:50:44 +0800

    Add files via upload

---
+++ zlg7290.h
@@ -0,0 +1,9 @@
+#ifndef __ZLG7290_H
+#define __ZLG7290_H
+
+#include "stm32f4xx_hal.h"
+
+void I2C_ZLG7290_Read(I2C_HandleTypeDef *I2Cx, uint8_t DevAddress, uint8_t MemAddress, uint8_t *pData, uint16_t Size);
+void I2C_ZLG7290_Write(I2C_HandleTypeDef *I2Cx, uint8_t DevAddress, uint8_t MemAddress, uint8_t *pData, uint16_t Size);
+
+#endif
